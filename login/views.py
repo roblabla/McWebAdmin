a6d7c08043c2412997a33bd39047446e79154c3a
Robin Lambertz <robinpub@googlemail.com>
2012-03-28 00:15:47 +0100

    Login works now. Moving on to dashboard.

--- login/views.py
+++ login/views.py
@@ -1,28 +1,30 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.shortcuts import render
 from django.template import RequestContext
 from django.template.base import Template
 
 def index(request):
-def validate(request):
     state = "notpost"
-username = password = ''
-if request.POST:
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    user = authenticate(username=username,password=password)
-    if user is not None:
-        if user.is_active:
-            login(request, user)
-            state="yes"
+    username = password = ''
+    loginornot = "base.html"
+    if request.POST:
+        loginornot = "empty.html"
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state="yes"
+            else:
+                state = "not active"
         else:
-            state = "not active"
-    else:
-        state = "no"
-thepage = RequestContext(request, {
-    'state': state,
-    'username': username,
+            state = "no"
+    thepage = RequestContext(request, {
+        'state': state,
+        'username': username,
+        'loginornot': loginornot
     })
-t = Template("login.html")
-return HttpResponse(t.render(thepage))
+    return render(request,"login.html",thepage)
--- mcwebadmin/settings.py
+++ mcwebadmin/settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/roblabla/.mcwebadmin/mcwebadmin.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'NAME': 'd:/mcmyadmin/mcmyadmin.db',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'testing123',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
